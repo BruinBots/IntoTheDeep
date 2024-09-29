4fee032cceea9fade626605f664e20e55557fe2e
Win10MC <easpecht09@gmail.com>
2024-09-29 13:56:35 -0400

    integrated drive code to the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -1,15 +1,55 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Hardware {
 
-    RevColorSensorV3 colorSensor;
+    public RevColorSensorV3 colorSensor;
+    public DcMotorEx leftFrontMotor;
+    public DcMotorEx rightFrontMotor;
+    public DcMotorEx leftBackMotor;
+    public DcMotorEx rightBackMotor;
+
     ColorDistanceSensor colorDistanceSensor;
     public Hardware(HardwareMap hardwareMap) {
         colorSensor = hardwareMap.get(RevColorSensorV3.class, "color_sensor");
         colorDistanceSensor = new ColorDistanceSensor(colorSensor);
+        leftFrontMotor = hardwareMap.get(DcMotorEx.class, "left_front");
+        rightFrontMotor = hardwareMap.get(DcMotorEx.class, "right_front");
+        leftBackMotor = hardwareMap.get(DcMotorEx.class, "left_back");
+        rightBackMotor = hardwareMap.get(DcMotorEx.class, "right_back");
+
+        // Reverse left motors
+        leftFrontMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotorEx.Direction.REVERSE);
     }
 
+    public void moveBotMecanum(double drive, double rotate, double strafe, double scaleFactor) {
+
+//        drive = rampUp(drive); // use S-curve to ramp up drive gradually
+
+        double[] wheelSpeeds = new double[4];
+        wheelSpeeds[0] = drive + strafe + rotate;  // left front
+        wheelSpeeds[1] = drive - strafe - rotate;  // right front
+        wheelSpeeds[2] = drive - strafe + rotate;  // left rear
+        wheelSpeeds[3] = drive + strafe - rotate;  // right rear
+
+        // finding the greatest power value
+        double maxMagnitude = Math.max(Math.max(Math.max(wheelSpeeds[0], wheelSpeeds[1]), wheelSpeeds[2]), wheelSpeeds[3]);
+
+        // dividing everyone by the max power value so that ratios are same (check if sdk automatically clips to see if go build documentation works
+        if (maxMagnitude > 1.0) {
+            for (int i = 0; i < wheelSpeeds.length; i++) {
+                wheelSpeeds[i] /= maxMagnitude;
+            }
+        }
+
+        // setting motor power and scaling down to preference
+        leftFrontMotor.setPower(wheelSpeeds[0] * scaleFactor);
+        rightFrontMotor.setPower(wheelSpeeds[1] * scaleFactor);
+        leftBackMotor.setPower(wheelSpeeds[2] * scaleFactor);
+        rightBackMotor.setPower(wheelSpeeds[3] * scaleFactor);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
@@ -3,9 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name="Color Sensor Test", group = "Iterative Opmode")
+@TeleOp(name="Main TeleOp", group = "Iterative Opmode")
 public class MainTeleop extends OpMode {
     Hardware map;
+
+    // drive values
+    double drive = 0.0;
+    double turn = 0.0;
+    double strafe = 0.0;
 
     @Override
     public void init() {
@@ -14,6 +19,21 @@ public class MainTeleop extends OpMode {
 
     @Override
     public void loop() {
+
+        drive = gamepad1.left_stick_y - gamepad2.left_stick_y;
+        strafe = gamepad2.left_stick_x - gamepad1.left_stick_x;
+        turn= gamepad1.right_stick_x + gamepad2.right_stick_x;
+
+        if (drive > 1) { drive = 1; }
+        if (strafe > 1) { strafe = 1; }
+        if (turn > 1) { turn = 1; }
+
+        strafe = Math.copySign(Math.pow(strafe, 2), strafe);
+        drive = Math.copySign(Math.pow(drive, 2), drive);
+        turn = Math.copySign(Math.pow(turn, 2), turn);
+
+        map.moveBotMecanum(drive, turn, strafe,  0.65); // actually move the robot
+
 
         map.colorDistanceSensor.loop();
 
