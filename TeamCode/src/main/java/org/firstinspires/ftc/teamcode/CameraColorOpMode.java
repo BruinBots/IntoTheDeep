2265f61925f6ff25654acd6a976026429518fd43
Win10MC <easpecht09@gmail.com>
2024-10-20 14:33:12 -0400

    set new color ranges for the colors because old ones weren't working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraColorOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraColorOpMode.java
@@ -27,6 +27,7 @@ import android.util.Size;
 //import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.SortOrder;
 //import com.qualcomm.robotcore.util.SortOrder;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -34,8 +35,10 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
 import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ColorSpace;
 import org.firstinspires.ftc.vision.opencv.ImageRegion;
 import org.opencv.core.RotatedRect;
+import org.opencv.core.Scalar;
 
 import java.util.List;
 
@@ -46,29 +49,40 @@ public class CameraColorOpMode extends LinearOpMode {
     public void runOpMode() {
         // Build color locators for each color
         ColorBlobLocatorProcessor blueLocator = new ColorBlobLocatorProcessor.Builder()
-                .setTargetColorRange(ColorRange.BLUE)
+                .setTargetColorRange(new ColorRange(
+                        ColorSpace.HSV,
+                        new Scalar(110, 97, 38), // Lower bound for yellow in HSV
+                        new Scalar(127, 217, 129)  // Upper bound for yellow in HSV
+                ))
                 .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
                 .setDrawContours(true)
                 .setRoi(ImageRegion.entireFrame())
-                .setBlurSize(5)
+                .setBlurSize(0)
                 .build();
 
         ColorBlobLocatorProcessor redLocator = new ColorBlobLocatorProcessor.Builder()
-                .setTargetColorRange(ColorRange.RED)
+                .setTargetColorRange(new ColorRange(
+                        ColorSpace.HSV,
+                        new Scalar(0, 217, 85), // Lower bound for yellow in HSV
+                        new Scalar(6, 255, 167)  // Upper bound for yellow in HSV
+                ))
                 .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
                 .setDrawContours(true)
                 .setRoi(ImageRegion.entireFrame())
-                .setBlurSize(5)
+                .setBlurSize(0)
                 .build();
 
         ColorBlobLocatorProcessor yellowLocator = new ColorBlobLocatorProcessor.Builder()
-                .setTargetColorRange(ColorRange.YELLOW)
+                .setTargetColorRange(new ColorRange(
+                        ColorSpace.HSV,
+                        new Scalar(11, 222, 147), // Lower bound for yellow in HSV
+                        new Scalar(27, 255, 255)  // Upper bound for yellow in HSV
+                ))
                 .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
                 .setDrawContours(true)
                 .setRoi(ImageRegion.entireFrame())
-                .setBlurSize(5)
+                .setBlurSize(0)
                 .build();
-
         // Create the VisionPortal with all color processors
         VisionPortal portal = new VisionPortal.Builder()
                 .addProcessor(blueLocator)
@@ -90,9 +104,9 @@ public class CameraColorOpMode extends LinearOpMode {
             List<ColorBlobLocatorProcessor.Blob> yellowBlobs = yellowLocator.getBlobs();
 
             // Filter blobs by size for each color
-            ColorBlobLocatorProcessor.Util.filterByArea(50, 20000, blueBlobs);
-            ColorBlobLocatorProcessor.Util.filterByArea(50, 20000, redBlobs);
-            ColorBlobLocatorProcessor.Util.filterByArea(50, 20000, yellowBlobs);
+            ColorBlobLocatorProcessor.Util.filterByArea(500, 20000, blueBlobs);
+            ColorBlobLocatorProcessor.Util.filterByArea(500, 20000, redBlobs);
+            ColorBlobLocatorProcessor.Util.filterByArea(500, 20000, yellowBlobs);
 
             telemetry.addLine(" Color  Area Density Aspect  Center");
 
