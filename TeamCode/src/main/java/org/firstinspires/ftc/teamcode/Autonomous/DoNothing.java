2d9b03a341f17014222e4f7538e2ec28aa27c887
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-11-05 20:37:17 -0500

    feat: Add TOC OpMode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DoNothing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DoNothing.java
@@ -3,13 +3,23 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name = "Do Nothing", group = "Umm...Nothing")
+@Autonomous(name = "Table of Contents", group = "Confused yet?")
 public class DoNothing extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        telemetry.addLine("Aren't you going to do anything for autonomous?");
-        telemetry.addLine("Nope! Bye.");
+        telemetry.addLine("._____.___...____.\n" +
+                "|_..._/._.\\./.___|\n" +
+                "..|.||.|.|.|.|....\n" +
+                "..|.||.|_|.|.|___.\n" +
+                "..|_|.\\___/.\\____|");
+        telemetry.addLine("Naming Convention for Autonomous OpMode's:");
+        telemetry.addLine("1. Location (Blue or Red) and (Near or Far)");
+        telemetry.addLine("2. Operations (in order)");
+        telemetry.addLine("Park - Self explanatory\nBasket - Place sample in basket\nSubmersible - Hang sample on submersible bar\nSamples - Pick up sample (automatically done if basket after submersible)");
+        telemetry.addLine("3. Good luck!");
+        telemetry.addLine("If this is too confusing, there's always TeleOp to make up for lost time...");
         telemetry.update();
+        sleep(10000);
     }
 }
