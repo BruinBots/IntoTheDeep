d8a0d677d339d5215c6f704d548c31c4a2f795f4
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-11-10 14:34:36 -0500

    feat: Add peripherals and turning to Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoBases/BaseAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoBases/BaseAuto.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.Autonomous.AutoBases;
 
+import static java.lang.Thread.sleep;
+
 import android.content.res.AssetManager;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -9,7 +12,9 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import org.firstinspires.ftc.teamcode.Arm;
 import org.firstinspires.ftc.teamcode.Hardware;
+import org.firstinspires.ftc.teamcode.Viper;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 import java.io.IOException;
@@ -17,6 +22,7 @@ import java.io.InputStream;
 import java.util.Properties;
 import java.util.Vector;
 
+@Config
 public class BaseAuto {
     private final HardwareMap hardwareMap;
     private final Telemetry telemetry;
@@ -39,6 +45,12 @@ public class BaseAuto {
     public static int SAMPLES_Y;
     public static int SUBMERSIBLE_X;
     public static int SUBMERSIBLE_Y;
+
+    public static int BASKET_ANGLE;
+    public static int SAMPLES_ANGLE;
+    public static int SUBMERSIBLE_ANGLE;
+
+    public static boolean peripherals_allowed = false;
 
     public BaseAuto(HardwareMap hardwareMap, Telemetry telemetry, Pose2d startingPosition, boolean blue, boolean near) {
         this.hardwareMap = hardwareMap;
@@ -66,6 +78,10 @@ public class BaseAuto {
             SAMPLES_Y = y_factor*Integer.parseInt(prop.getProperty("samples.y"));
             SUBMERSIBLE_X = Integer.parseInt(prop.getProperty("submersible.x"));
             SUBMERSIBLE_Y = y_factor*Integer.parseInt(prop.getProperty("submersible.y"));
+
+            BASKET_ANGLE = conditionallyInvertAngle(Integer.parseInt(prop.getProperty("basket.angle")), !blue);
+            SAMPLES_ANGLE = conditionallyInvertAngle(Integer.parseInt(prop.getProperty("samples.angle")), !blue);
+            SUBMERSIBLE_ANGLE = conditionallyInvertAngle(Integer.parseInt(prop.getProperty("submersible.angle")), !blue);
         }
         catch (IOException ex) {
             ex.printStackTrace();
@@ -81,6 +97,25 @@ public class BaseAuto {
         return new Vector2d(pose.getX(), pose.getY());
     }
 
+    public void wait(double seconds) {
+        try {
+            sleep((long) (seconds*1000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void wait(int seconds) { wait((double)seconds); }
+
+    public int invertAngle(int angle) { return (angle+180>=360)?(angle-180):(angle+180); }
+    public int conditionallyInvertAngle(int angle, boolean invert) { return invert?invertAngle(angle):angle; }
+
+    public Pose2d posePlusAngle(Trajectory traj, int angle) { return posePlusAngle(traj.end(), angle); }
+
+    public Pose2d posePlusAngle(Pose2d pose, int angle) {
+        return pose.plus(new Pose2d(0, 0, Math.toRadians(angle)));
+    }
+
     ///// ---------- RUN ---------- /////
 
     public static enum AutoOperation {
@@ -94,6 +129,7 @@ public class BaseAuto {
         Pose2d curPos = startPose;
         int count = ops.length;
         int i = 0;
+        curPos = highway(curPos);
         for (AutoOperation op: ops) {
             switch (op) {
                 case PARK:
@@ -160,12 +196,25 @@ public class BaseAuto {
     }
 
     public Pose2d basket(Pose2d startPose) {
-        Trajectory traj = drive.trajectoryBuilder(startPose)
+        drive.turn(BASKET_ANGLE);
+        Trajectory traj = drive.trajectoryBuilder(posePlusAngle(startPose, BASKET_ANGLE))
                 .lineToConstantHeading(basket_v())
                 .build();
         drive.followTrajectory(traj);
 
         // TODO: Place sample in basket
+        if (peripherals_allowed) {
+            // Lift viper slides
+            bot.viper.move(Viper.MAX_VIPER_POS);
+            wait(0.5);
+            // Drop pixel
+            bot.basket.setOpen();
+            wait(0.5);
+            bot.basket.setClosed();
+            wait(0.5);
+            // Lower viper slides
+            bot.viper.move(Viper.MIN_VIPER_POS);
+        }
 
         return traj.end();
     }
@@ -176,12 +225,33 @@ public class BaseAuto {
     public Pose2d samples(Trajectory startTraj) { return samples(startTraj.end()); }
 
     public Pose2d samples(Pose2d startPose) {
-        Trajectory traj = drive.trajectoryBuilder(startPose)
+        drive.turn(SAMPLES_ANGLE);
+        Trajectory traj = drive.trajectoryBuilder(posePlusAngle(startPose, SAMPLES_ANGLE))
                 .lineToConstantHeading(samples_v())
                 .build();
         drive.followTrajectory(traj);
 
         // TODO: Pick up sample
+        if (peripherals_allowed) {
+            // Lower arm
+            bot.arm.moveWrist(Arm.MAX_WRIST_POS);
+            bot.arm.moveArm(Arm.MAX_ARM_POS);
+            wait(0.5);
+            // Pick up sample
+            bot.intake.engage();
+            wait(0.5);
+            // Retract arm
+            bot.arm.moveArm(Arm.MIN_ARM_POS);
+            bot.arm.moveWrist(Arm.MIN_WRIST_POS);
+            wait(0.5);
+            // Lower viper
+            bot.viper.move(Viper.MIN_VIPER_POS);
+            bot.basket.setOpen();
+            // Handoff
+            bot.intake.standby();
+            wait(1);
+            bot.basket.setClosed();
+        }
 
         return traj.end();
     }
@@ -192,13 +262,30 @@ public class BaseAuto {
     public Pose2d submersible(Trajectory startTraj) { return submersible(startTraj.end()); }
 
     public Pose2d submersible(Pose2d startPose) {
-        Trajectory traj = drive.trajectoryBuilder(startPose)
+        drive.turn(SUBMERSIBLE_ANGLE);
+        Trajectory traj = drive.trajectoryBuilder(posePlusAngle(startPose, SUBMERSIBLE_ANGLE))
                 .lineToConstantHeading(submersible_v())
                 .build();
         drive.followTrajectory(traj);
 
         // TODO: Place specimen on horizontal bar
-
+        if (peripherals_allowed) {
+            // Lift viper
+            bot.viper.move(Viper.MAX_VIPER_POS);
+            wait(1);
+            // Basket to middle position
+            bot.basket.setMiddle();
+            wait(0.5);
+            // Slowly lower viper
+            bot.viper.move(Viper.MAX_VIPER_POS-1000);
+            wait(2);
+            // Open basket
+            bot.basket.setOpen();
+            wait(0.5);
+            // Lower viper
+            bot.viper.move(Viper.MIN_VIPER_POS);
+            wait(1);
+        }
         return traj.end();
     }
 }
