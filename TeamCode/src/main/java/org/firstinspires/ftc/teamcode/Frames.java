369eb178fa6b924e638c880c8384641215d7c35d
Win10MC <easpecht09@gmail.com>
2024-12-04 20:34:53 -0500

    add slow/fast mode and adjust position for top basket button

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Frames.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Frames.java
@@ -181,7 +181,7 @@ public class Frames {
     };
 
     public Frame[] topBasketFrames = new Frame[] {
-        new ViperFrame(3200),
+        new ViperFrame(6100),
     };
 
     public Frame[] uncurlFrames = new Frame[] {
