850ce2455d425c86416b77f9bb78de8261127f7d
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2025-01-28 20:31:47 -0500

    feat: ClawMap and ClawMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawMode.java
@@ -7,8 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class ClawMode extends OpMode {
     public ClawMap claw1;
     public ClawMap claw2;
-
-
+\
     @Override
     public void init() {
         claw1 = new ClawMap(hardwareMap, telemetry, gamepad1, "1");
